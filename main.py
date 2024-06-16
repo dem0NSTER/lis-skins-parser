313d348d8573a498214baad244a6f1990dc5e000
Pavel <pavel.fedorenko0602@gmail.com>
2024-06-16 11:07:53 +0700

    fix some bugs

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def main(lis_skins_base_url, fast_profit=False):
             time.sleep(4)
             print('[INFO] first time ended')
 
-        if not is_empty_json('/json/lis_skins.json'):
+        if not is_empty_json(f'{PATH_TO_PROJECT}/json/lis_skins.json'):
             print('[INFO] start not processed elements')
             not_processed_elements(fast_profit=fast_profit)
             print('[INFO] end not processed elements')
@@ -26,15 +26,15 @@ def main(lis_skins_base_url, fast_profit=False):
 
         print('[INFO] lis_skins.json is empty. The only thing left to do is to process the items not found')
 
-        if not is_empty_json('/json/cant_find.json'):
+        if not is_empty_json(f'{PATH_TO_PROJECT}/json/cant_find.json'):
             cant_find_elements(fast_profit=fast_profit)
 
-        if is_empty_json('/json/lis_skins.json') and is_empty_json('/json/cant_find.json'):
+        if is_empty_json(f'{PATH_TO_PROJECT}/json/lis_skins.json') and is_empty_json(f'{PATH_TO_PROJECT}/json/cant_find.json'):
             print('[INFO] everythig is ok')
             break
 
 
 if __name__ == '__main__':
     url = 'https://lis-skins.ru/market/csgo/?category_id=19%2C11%2C32%2C3%2C7%2C28%2C10&price_from=350&price_to=1000&hold=-1&is_without_souvenir=1'
-    fast_url = 'https://lis-skins.ru/market/csgo/?type_id=46%2C48%2C49%2C47%2C50%2C51&price_from=50&price_to=70&hold=-1'
+    fast_url = 'https://lis-skins.ru/market/csgo/?sort_by=hot&type_id=46%2C48%2C49%2C47%2C50%2C51&price_from=10&price_to=15&hold=-1'
     main(lis_skins_base_url=fast_url, fast_profit=True)
